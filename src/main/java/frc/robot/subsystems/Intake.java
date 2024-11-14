fa3ff36749f2992a0ba960268b4dd00ac4a5f134
ka930 <77208047+ka930@users.noreply.github.com>
2024-11-14 18:50:04 -0500

    spotless - reformat

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -6,19 +6,19 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-    private final static int INTAKE_ID = 9;
+  private static final int INTAKE_ID = 9;
 
-    private final static double INTAKE_IN_VOLTS = 7.0;
+  private static final double INTAKE_IN_VOLTS = 7.0;
 
-    private final TalonFX motor = new TalonFX(INTAKE_ID);
+  private final TalonFX motor = new TalonFX(INTAKE_ID);
 
-    private final VoltageOut control = new VoltageOut(0);
+  private final VoltageOut control = new VoltageOut(0);
 
-    private void setMotorPower(double volts) {
-        motor.setControl(control.withOutput(volts));
-    }
+  private void setMotorPower(double volts) {
+    motor.setControl(control.withOutput(volts));
+  }
 
-    public Command moveIntakeIn() {
-        return startEnd(() -> setMotorPower(INTAKE_IN_VOLTS), () -> setMotorPower(0));
-    }
+  public Command moveIntakeIn() {
+    return startEnd(() -> setMotorPower(INTAKE_IN_VOLTS), () -> setMotorPower(0));
+  }
 }
