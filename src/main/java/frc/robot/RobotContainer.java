945ae0f72bdf5d1b88821a1b41bba449788ea369
justjai333 <145171450+justjai333@users.noreply.github.com>
2024-11-14 20:14:30 -0500

    pathplanner

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,9 +8,11 @@ import static edu.wpi.first.units.Units.*;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.generated.TunerConstants;
@@ -18,6 +20,7 @@ import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Intake;
 
 public class RobotContainer {
+
   public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
   public final Intake intake = new Intake();
   private final CommandXboxController joystick = new CommandXboxController(0);
@@ -39,7 +42,11 @@ public class RobotContainer {
 
   public RobotContainer() {
     configureBindings();
+    autoChooser = AutoBuilder.buildAutoChooser("Tests");
+    SmartDashboard.putData("Auto Mode", autoChooser);
   }
+
+  private final SendableChooser<Command> autoChooser;
 
   private void configureBindings() {
     // Note that X is defined as forward according to WPILib convention,
@@ -83,6 +90,6 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return autoChooser.getSelected();
   }
 }
--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -2,20 +2,18 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Elevator extends SubsystemBase{
-    private static final int ELEVATOR_ID = 6;
+public class Elevator extends SubsystemBase {
+  private static final int ELEVATOR_ID = 6;
 
   private static final double ELEVATOR_DOWN_POSITION = 8.0;
   private static final double ELEVATOR_UP_POSITION = 16.0;
 
-
   private final TalonFX motor = new TalonFX(ELEVATOR_ID);
 
-  private final PositionVoltage control = new PositionVoltage (0);
+  private final PositionVoltage control = new PositionVoltage(0);
 
   private void setMotorPosition(double volts) {
     motor.setControl(control.withPosition(volts));
